108517bce3f03d448de9c1c22eeba1792005f22f
as-galkin <demotivatoris@gmail.com>
2021-10-25 17:18:17 +0300

    First commit. Export CleanSwift architecture scheme.

---
+++ ForaSoftInterview/Services/Models/SearchResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResponse.swift
+//  ForaSoftInterview
+//
+//  Created by Александр Галкин on 25.10.2021.
+//
+
+import Foundation
+
+struct SearchResponse {
+    
+}
---
+++ ForaSoftInterview/Services/NetworkLayer.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkLayer.swift
+//  ForaSoftInterview
+//
+//  Created by Александр Галкин on 25.10.2021.
+//
+
+import Foundation
+
+class NetworkLayer {
+    public func fetchResult(searchText: String, completion: @escaping (_ searchResult: SearchResponse?) -> Void) {
+        
+    }
+}
