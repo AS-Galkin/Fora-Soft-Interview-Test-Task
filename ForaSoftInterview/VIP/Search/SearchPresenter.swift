1bb4052449c6d521bef5960bae64717f022f0b66
as-galkin <demotivatoris@gmail.com>
2021-10-29 09:45:17 +0300

    Added alphabet sort for Albums

--- ForaSoftInterview/VIP/Search/SearchPresenter.swift
+++ ForaSoftInterview/VIP/Search/SearchPresenter.swift
@@ -15,14 +15,21 @@ protocol SearchPresentationLogic {
 class SearchPresenter: SearchPresentationLogic {
     weak var viewController: SearchDisplayLogic?
     
+    //MARK: - Preparation data for displaying
     func presentData(response: Search.Model.Response.ResponseType) {
         switch response {
         case .presentAlbums(let response):
+            
+            //Convert ResponseViewModel to AlbumViewModel. And Aphabet sorting.
             guard let cells = response?.results?.map({ album -> AlbumViewModel.Album in
                 return AlbumViewModel.Album.init(imageUrl: album.artworkUrl100, artistName: album.artistName, albumName: album.collectionName, albumId: album.collectionId)
+            }).sorted(by: { lhs, rhs in
+                guard let left = lhs.albumName, let right = rhs.albumName else { return false}
+                return left < right
             }) else { return }
-            
+
             let viewModel = AlbumViewModel.init(cells: cells)
+            
             viewController?.displayData(viewModel: .displayAlbums(albumViewModel: viewModel))
             break
         case .presentActivityIndicator:
